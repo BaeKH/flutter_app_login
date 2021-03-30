4309af076cf6d36109390b6d64ec64b67519305b
bgh <bgh0611@naver.com>
2021-03-30 23:25:42 +0900

    1.로그인 화면 배치 작업중

--- lib/pages/auth_page.dart
+++ lib/pages/auth_page.dart
@@ -1,3 +1,4 @@
+
 import 'package:flutter/material.dart';
 import 'package:flutter_app_login/provider/page_notifier.dart';
 import 'package:provider/provider.dart';
@@ -20,22 +21,90 @@ class AuthWidget extends StatefulWidget {
 }
 
 class _AuthWidgetState extends State<AuthWidget> {
+
+  GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  TextEditingController _emailController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
+  TextEditingController _confirmPasswordController = TextEditingController();
+
+  OutlineInputBorder _border = OutlineInputBorder(
+    borderRadius: BorderRadius.circular(8),
+    borderSide: BorderSide(color: Colors.transparent, width: 0),
+  );
+
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: InkWell(
-        onTap: () {
-          Provider.of<PageNotifier>(context, listen: false).goToMain();
-        },
-          child: Container(
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                fit: BoxFit.cover,
-                  image: AssetImage('assets/image.gif')
-              )
+      child: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            fit: BoxFit.cover,
+              image: AssetImage('assets/image.gif')
+          )
+        ),
+        child: Scaffold(
+          backgroundColor: Colors.transparent,
+            body: SafeArea(
+              child: Form(
+                key: _formKey,
+                child: ListView(
+                  padding: EdgeInsets.all(16),
+                  children: [
+                    SizedBox(
+                        height: 16
+                    ),
+                    CircleAvatar(
+                      backgroundColor: Colors.white54,
+                      radius: 36,
+                      child: Text("LogIn"),
+                    ),
+                    SizedBox(
+                      height: 16,
+                    ),
+                    _buildTextFormField("Email Address", _emailController),
+                    SizedBox(
+                      height: 8,
+                    ),
+                    _buildTextFormField("Password", _passwordController),
+                    SizedBox(
+                      height: 8,
+                    ),
+                    _buildTextFormField("Confirm Password", _confirmPasswordController),
+                    SizedBox(
+                      height: 16,
+                    ),
+                    FlatButton(onPressed: (){}, child: Text("Login"),)
+                  ],
+                ),
+              ),
             ),
-          ),
-      ),
-    );
+        ),
+        ),
+      );
+  }
+
+  TextFormField _buildTextFormField(String labelText, TextEditingController controller) {
+    return TextFormField(
+                    cursorColor: Colors.white,
+                    controller: controller,
+                    validator: (text){
+                      if(text == null || text.isEmpty) {
+                        return "입력창이 비어있어요!";
+                      }
+
+                      return null;
+                    },
+                    style: TextStyle(color: Colors.white),
+                    decoration: InputDecoration(
+                      labelText: labelText,
+                      filled: true,
+                      fillColor: Colors.black45,
+                      border: _border,
+                      enabledBorder: _border,
+                      focusedBorder: _border,
+                      labelStyle: TextStyle(color: Colors.white),
+                    ),
+                  );
   }
 }
